a61748f6a431717855183b2b0c6f3f728a39f6b7
Binay Shaw <binayshaw7777@gmail.com>
2022-04-21 16:07:54 +0530

    Signup and Signin UI done 🔥

--- app/src/main/java/com/geekymusketeers/gail/authentication/SignUpScreen.java
+++ app/src/main/java/com/geekymusketeers/gail/authentication/SignUpScreen.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.geekymusketeers.gail.R;
 
-public class SignInScreen extends AppCompatActivity {
+public class SignUpScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in_screen);
+        setContentView(R.layout.activity_sign_up_screen);
     }
 }
