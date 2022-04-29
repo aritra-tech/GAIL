bceab5096c20a624fe71fa8891773cf195d43ab6
JOYPAUL2001 <joydeepp2001@gmail.com>
2022-04-30 02:58:06 +0530

    attendance scan done

--- app/src/main/java/com/geekymusketeers/gail/Attendance_Scan.java
+++ app/src/main/java/com/geekymusketeers/gail/Attendance_Scan.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Result_view extends AppCompatActivity {
+public class Attendance_Scan extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result_view);
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_attendance_scan);
     }
 }
--- app/src/main/java/com/geekymusketeers/gail/Profile_view.java
+++ app/src/main/java/com/geekymusketeers/gail/Profile_view.java
@@ -10,5 +10,6 @@ public class Profile_view extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+        getSupportActionBar().hide();
     }
 }
